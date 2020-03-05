bf16d97c6fc2d523083f48a381f64d759fd1c026
Samuel Atkins <s.m.atkins73@gmail.com>
2020-03-04 20:54:16 -0500

    04/03/2020-Atkins-20:54:16 Added new file that details installation process for IntelAI's he-transformer

---
+++ he-transformer-setup-instructions.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# This file runs the appropriate commands to replice the he-transformer repository located at:
+# https://github.com/IntelAI/he-transformer
+cd ~
+sudo apt install g++
+sudo apt update && sudo apt install -y python3-pip virtualenv python3-numpy python3-dev python3-wheel git unzip wget sudo bash-completion build-essential cmake software-properties-common git wget patch diffutils libtinfo-dev autoconf libtool doxygen graphviz
+wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
+sudo apt-add-repository "deb http://apt.llvm.org/bionic/ llvm-toolchain-bionic-9 main"
+sudo apt-get update && sudo apt install -y clang-9 clang-tidy-9 clang-format-9
+git clone https://github.com/IntelAI/he-transformer.git
+sudo apt-get clean autoclean
+sudo apt-get autoremove -y
+sudo pip3 install --upgrade pip setuptools virtualenv==16.1.0
+sudo pip3 install cmake --upgrade
+sudo -H pip3 install cmake --upgrade
+wget https://github.com/bazelbuild/bazel/releases/download/0.25.2/bazel-0.25.2-installer-linux-x86_64.sh
+chmod +x ./bazel-0.25.2-installer-linux-x86_64.sh 
+sudo bash ./bazel-0.25.2-installer-linux-x86_64.sh 
+export PATH=$PATH:~/bin
+source ~/.bashrc
+export HE_TRANSFORMER=$(pwd)
+cd ~/he-transformer/
+mkdir build
+cd build
+sudo cmake .. -DCMAKE_CXX_COMPILER=clang++-9 -DCMAKE_C_COMPILER=clang-9
+sudo make install
+source $HE_TRANSFORMER/build/external/venv-tf-py3/bin/activate
+pip install numpy
